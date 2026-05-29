bf21978dcdcb954c3de2b3cbcec3c0baf8cb95b0
trung-tin-dev <tatin3469@gmail.com>
2026-05-29 12:23:40 +0700

    add dotenv

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,17 +1,20 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.get("/api/health", (req: Request, res: Response) => {
   res.json({
     status: "success",
-    message: "Server Express (TypeScript) trong Turborepo đang chạy tốt!",
+    message: "Server Express is running in Turborepo",
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Backend đang chạy tại: http://localhost:${PORT}`);
+  console.log(`Backend đang chạy tại: http://localhost:${PORT}`);
 });
